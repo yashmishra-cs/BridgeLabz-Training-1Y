c287d6922541a5013191ed55642ea3d09b6bd7b9
Yash Mishra <yk2551499@gmail.com>
2026-01-13 14:19:31 +0530

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class AddTwoNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
@@ -8,4 +8,5 @@ public class Main {
         System.out.println("Sum = " + sum);
     }
 }
+
 
