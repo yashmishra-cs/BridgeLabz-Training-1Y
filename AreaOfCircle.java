deca18a3f5fc03d3e684da8de291c726488a67f0
Yash Mishra <yk2551499@gmail.com>
2026-01-13 14:31:39 +0530

    Update AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class AreaOfCircle {
+public class AreaOfCirce {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 double r = sc.nextDouble();
@@ -9,3 +9,4 @@ System.out.println("Area Of Circle:" + ar);
 }
 
 }
+
