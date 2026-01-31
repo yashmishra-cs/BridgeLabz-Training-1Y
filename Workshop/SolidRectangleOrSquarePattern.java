521d300a16a34e283278f415a041e8ed8f5a3291
Yash Mishra <yk2551499@gmail.com>
2026-01-31 17:48:11 +0530

    Added to workshop

--- Workshop/SolidRectangleOrSquarePattern.java
+++ Workshop/SolidRectangleOrSquarePattern.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class SolidRectangleOrSquare {
+public class SolidRectangleOrSquarePattern {
 public static void main(String[] args) {
 for (int i=1;i<=5;i++){
 for (int j=1;j<=5;j++){
