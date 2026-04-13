539a984029b6f5937de0bdc16c4941ca9cf7a912
Yash Mishra <yk2551499@gmail.com>
2026-04-13 15:25:37 +0530

    Added

--- Workshop/Frequency.java
+++ Workshop/Frequency.java
@@ -4,22 +4,25 @@ import java.util.Scanner;
 public class Frequency {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int a = sc.nextInt();
         int[] arr = new int[a];
-        for(int i=0;i<a;i++){
-            i = sc.nextInt();
+
+        for(int i = 0; i < a; i++){
+            arr[i] = sc.nextInt();
         }
-        int[] freq = new int[100];
+
         Arrays.sort(arr);
-        for(int j=0;j<arr.length;j++){
+
+        for(int i = 0; i < arr.length; i++){
             int count = 1;
-            for(int k=j+1;k<arr.length-1;k++){
-                if(arr[j]==arr[k]){
-                    count++;
-                }
+
+            while(i < arr.length - 1 && arr[i] == arr[i + 1]){
+                count++;
+                i++;
             }
-            freq[j]=count;
+
+            System.out.println(arr[i] + " -> " + count);
         }
-        System.out.println(freq);
     }
 }
